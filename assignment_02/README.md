9c1e5adfa00bdaba1449c6e3aa4f8e24e6284e69
Lee Morin <Lealand.Morin@ucf.edu>
2021-01-19 22:38:09 -0500

    Uploaded Assignment 2

--- assignment_02/README.md
+++ assignment_02/README.md
@@ -1,4 +1,4 @@
 # Assignment 2
 
-Upload your function my_functions.py to a folder with (exactly!) the same name (assignment_02)
+Upload your script my_functions.py to a folder with (exactly!) the same name (assignment_02)
 within your repository. 
---
+++ assignment_02/my_functions.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+##################################################
+#
+# ECP 3004: Python for Business Analytics
+#
+# Name: 
+#
+# Date:
+# 
+##################################################
+#
+# Sample Script for Assignment 2: 
+# Function Definitions
+#
+##################################################
+"""
+
+
+##################################################
+# Import Required Modules
+##################################################
+
+# import name_of_module
+
+
+##################################################
+# Function Definitions
+##################################################
+
+# Exercise 1
+
+def average(num1: float, num2: float) -> float:
+    """Return the average of num1 and num2.
+
+    >>> average(10,20)
+    15.0
+    >>> average(2.5, 3.0)
+    2.75
+    """
+
+    return num1 + num2 / 2
+
+
+
+# Define the rest of your functions for Exercises 2-6.
+ 
+
+
+
+
+
+
+##################################################
+# Run the examples to test these functions
+##################################################
+
+
+# Test the examples and print the results. 
+
+
+print("#" + 50*"-")
+print("Testing my Examples for Exercise 1.")
+
+print("#" + 50*"-")
+print("Exercise 1, Example 1:")
+print("Evaluating average(10,20)")
+print("Expected: " + str(15.0))
+print("Got: " + str(average(10,20)))
+
+
+print("#" + 50*"-")
+print("Exercise 1, Example 2:")
+print("Evaluating average(2.5, 3.0)")
+print("Expected: " + str(2.75))
+print("Got: " + str(average(2.5, 3.0)))
+
+
+print("#" + 50*"-")
+print("Exercise 1, Example 3:")
+# ...
+
+print("#" + 50*"-")
+print("Testing my Examples for Exercise 2.")
+
+
+# ...
+
+# Continue with the rest of your examples.
+# Test all functions with three examples each. 
+
+# Choose good examples that will test interesting cases. 
+# Make sure they all work correctly. 
+
+
+##################################################
+# End
+##################################################
