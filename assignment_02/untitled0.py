f0e29015c6c3bcf0d9c4d4aee9a60530b80d08a5
lu012848 <77599018+lu012848@users.noreply.github.com>
2021-01-31 23:56:17 -0500

    Add files via upload

---
+++ assignment_02/untitled0.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 23:05:03 2021
+
+@author: lp1
+"""
+
+# -*- coding: utf-8 -*-
+"""
+##################################################
+#
+# ECP 3004: Python for Business Analytics
+#
+# Name: Luis Perez
+#
+# Date: 1/31/2021
+# 
+##################################################
+#
+# Sample Script for Assignment 2: 
+# Function Definitions
+#
+##################################################
+"""
+
+
+##################################################
+# Import Required Modules
+##################################################
+
+# import name_of_module
+
+
+##################################################
+# Function Definitions
+##################################################
+
+# Exercise 1
+
+def average(num1: float, num2: float) -> float:
+    """Return the average of num1 and num2.
+    >>> average(10,20)
+    15.0
+    >>> average(2.5, 3.0)
+    2.75
+    """
+
+    return (num1 + num2) / 2
+
+
+
+# Define the rest of your functions for Exercises 2-6.
+ 
+def area_of_circle(a):
+        
+
+
+
+
+
+##################################################
+# Run the examples to test these functions
+##################################################
+
+
+# Test the examples and print the results. 
+
+
+print("#" + 50*"-")
+print("Testing my Examples for Exercise 1.")
+
+print("#" + 50*"-")
+print("Exercise 1, Example 1:")
+print("Evaluating average(10,20)")
+print("Expected: " + str(15.0))
+print("Got: " + str(average(10,20)))
+
+
+print("#" + 50*"-")
+print("Exercise 1, Example 2:")
+print("Evaluating average(2.5, 3.0)")
+print("Expected: " + str(2.75))
+print("Got: " + str(average(2.5, 3.0)))
+
+
+print("#" + 50*"-")
+print("Exercise 1, Example 3:")
+# ...
+
+print("#" + 50*"-")
+print("Testing my Examples for Exercise 2.")
+
+
+# ...
+
+# Continue with the rest of your examples.
+# Test all functions with three examples each. 
+
+# Choose good examples that will test interesting cases. 
+# Make sure they all work correctly. 
+
+
+##################################################
+# End
+##################################################
