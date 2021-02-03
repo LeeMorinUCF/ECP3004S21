dbc188e94a491cd8d32ede83ba19ad5c4f811806
Lee Morin <Lealand.Morin@ucf.edu>
2021-02-03 10:58:23 -0500

    Uploaded material for Assignment 3

---
+++ assignment_03/my_A3_functions.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+##################################################
+#
+# ECP 3004: Python for Business Analytics
+#
+# Name: 
+#
+# Date:
+# 
+##################################################
+#
+# Sample Script for Assignment 3: 
+# Function Definitions
+#
+##################################################
+"""
+
+
+##################################################
+# Import Required Modules
+##################################################
+
+# import name_of_module
+
+
+##################################################
+# Function Definitions
+##################################################
+
+# Only function definitions here - no other calculations. 
+
+# Exercise 1
+
+def name_of_function(arg_1: type, arg_2: type) -> type:
+    """Description of function.
+
+    >>> don_t_forget_the_examples(0, 0)
+    answer_you_expect
+    """
+    answer = None # Replace with body of function
+
+    return answer
+
+
+
+# Define the rest of your functions for Exercises 2-4.
+ 
+
+# Exercise 2
+
+
+# Exercise 3
+
+
+# Exercise 4
+
+
+
+# Only function definitions above this point. 
+
+
+##################################################
+# Run the examples to test these functions
+##################################################
+
+
+# Test the examples and print the results. 
+
+
+print("#" + 50*"-")
+print("Testing my Examples for Exercise 1.")
+
+print("#" + 50*"-")
+print("Exercise 1, Example 1:")
+print("Evaluating name_of_function(0, 0)")
+print("Expected: " + str(answer_you_expect))
+print("Got: " + str(name_of_function(0, 0)))
+
+
+print("#" + 50*"-")
+print("Exercise 1, Example 2:")
+# ...
+
+
+print("#" + 50*"-")
+print("Exercise 1, Example 3:")
+# ...
+
+
+# ...
+
+# Continue with the rest of your examples.
+# Test all functions with three examples each. 
+
+# Choose good examples that will test interesting cases. 
+# Make sure they all work correctly. 
+
+
+print("#" + 50*"-")
+print("Testing my Examples for Exercise 2.")
+
+
+# ...
+
+
+##################################################
+# End
+##################################################
